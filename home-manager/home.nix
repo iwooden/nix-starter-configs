615ba71e3fa3e29ad97a56bf28a834c87ff2a57d
Isaac Wooden <iwooden@protonmail.com>
2024-05-30 13:01:13 -0700

    init changes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,6 +2,7 @@
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
+  outputs,
   lib,
   config,
   pkgs,
@@ -9,8 +10,11 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
@@ -19,7 +23,12 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
       # Or define it inline, for example:
@@ -33,15 +42,13 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
     };
   };
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "iwooden";
+    homeDirectory = "/home/iwooden";
   };
 
   # Add stuff for your user as you see fit:
@@ -53,8 +60,9 @@
   programs.git.enable = true;
 
   # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+  # Disabling cause this prooobably breaks on WSL
+  # systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
 }
---
+++ nixos/wsl.nix
@@ -0,0 +1,13 @@
+{ ... }: {
+  imports = [ ./default-stuff.nix ];
+  nixpkgs.hostPlatform = "x86_64-linux";
+  networking.hostName = "nix-wsl";
+
+  # wsl-specific config
+  wsl.enable = true;
+  wsl.defaultUser = "iwooden";
+
+  # don't change this
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11";
+}
