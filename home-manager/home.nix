f6dd53b1335f7925e7f454aa82a4eb1bf2fb44c4
Isaac Wooden <iwooden@protonmail.com>
2024-05-30 15:53:29 -0700

    try this sysd thing

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -105,7 +105,7 @@
 
   # Nicely reload system units when changing configs
   # Disabling cause this prooobably breaks on WSL
-  # systemd.user.startServices = "sd-switch";
+  systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
